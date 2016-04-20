1bbb268c072217b100737901fb38d983b98d67b9
dsabolo <diego.sabolo@gmail.com>
2016-04-20 12:53:36 -0300

    initial commit

---
+++ app_buscador.php
@@ -0,0 +1,44 @@
+<div class="app_container">
+	<form class="form-horizontal col col-md-4" >
+<fieldset>
+
+<!-- Form Name -->
+
+
+<!-- Text input-->
+<div class="form-group">
+<label>Nombre</label>
+  <input id="nombre" name="nombre" type="text" class="form-control input-md" required="">
+
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+	<label>Email</label>	
+  <input id="email" name="email" type="mail" class="form-control input-md" required="">
+</div>
+
+<!-- Select Basic -->
+<div class="form-group">
+	<label>Provincia</label>
+    <select id="selectbasic" name="selectbasic" class="form-control">
+      <option value="" selected>Provincia</option>
+      <option value="2">Option two</option>
+    </select>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+ <label>Ciudad</label>
+  <input id="ciudad" name="ciudad" type="text" class="form-control input-md">
+</div>
+
+<!-- Button -->
+<div class="form-group">
+    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Buscar</button>
+</div>
+
+</fieldset>
+</form>
+
+</div>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>CONABIP: Comisión Nacional de Bibliotecas Populares</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Le styles -->
+    <link rel="stylesheet" type="text/css" href="dist/css/metro-bootstrap.min.css">
+    <link rel="stylesheet" href="styles/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/swiper-min.css">
+    <link rel="shortcut icon" href="http://www.conabip.gob.ar/sites/all/themes/cnbp/favicon.ico" type="image/vnd.microsoft.icon" />
+
+  </head>  
+  <body >
+  
+  <div id="modal">
+      <a id="modalClose" href="#"> <i class="fa fa-times" aria-hidden="true"></i></a>
+      <div id="modalCnt"></div>
+  </div> 
+  <div id="wrapper">
+  
+  <div id="mainNav" class="navbar navbar-side navbar navbar-fixed-top" role="navigation">
+      
+        <div class="collapse navbar-collapse">
+          <div class="thumbnail tile tile-medium tile-buscador">
+            <a href="#"  data-title="Buscador BP" data-lead="Buscador BP" data-bg="#0096D5" data-app="app_buscador" id="lnkBuscador">
+              <img src="images/twittertile.png">
+              <h3>
+              Buscador BP
+              </h3>
+            </a>
+          </div>
+         
+          <div class="thumbnail tile tile-medium tile-canal">
+            <a href="#"  data-title="Canal BP" data-lead="Videos BP" data-bg="#DDA333" data-app="app_canal">
+              <img src="images/twittertile.png">
+              <h2>
+              Canal BP
+              </h2>
+            </a>
+          </div>
+          <div class="thumbnail tile tile-medium tile-radio">
+            <a href="#"  data-title="Radio BP" data-lead="Radio BP" data-bg="#79368C" data-app="app_radio">
+              <img src="images/twittertile.png">
+              <h2>
+              Radio BP
+              </h2>
+            </a>
+          </div>
+
+         <div class="thumbnail tile tile-medium tile-revista">
+            <a href="#" data-title="Revista BP" data-lead="Revista BP" data-bg="#C43015" data-app="app_revista">
+              <img src="images/twittertile.png">
+              <h2>
+              Revista BP
+              </h2>
+            </a>
+          </div>
+  
+
+
+
+          
+        </div>
+		
+	
+		
+    </div>
+	<div id="wrapper-container">
+    <div class="container">
+    <div id="radioCnt"></div>
+    
+        <h1 id="sectionTitle">CONABIP</h1>
+       
+     
+      <div id="mainContent">
+          
+          
+      
+      </div>
+    </div><!-- /.container -->
+	</div>
+	</div>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="scripts/vendor/bootstrap.min.js"></script>
+    <script type="text/javascript" src="scripts/metro-docs.js"></script>
+    <script type="text/javascript" src="scripts/swiper-min.js"></script>
+    <script type="text/javascript" src="scripts/custom.js"></script>
+  </body>
+</html>
